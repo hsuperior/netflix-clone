66271a4df0c299cd82a01018d2c29032d14adc35
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-06 16:59:58 -0500

    starting point for the Banner Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Row from "./Components/Row";
+import Banner from "./Components/Banner";
 import requests from "./requests";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey! Let's build Netflix Clone</h1>
+      {/** Nav */}
+
+      {/** Banner */}
+
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
