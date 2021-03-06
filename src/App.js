0ccfb84a5bf79bb5b52a86207d2e72677062d3d7
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-05 21:25:55 -0500

    Add in Netflix Originals, Trending Now, Top Rated Rows in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Row from "./Components/Row";
+import requests from "./requests";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey! Let's build Netflix Clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
     </div>
   );
 }
