63f288fb4cc1826097821e36b4b2c541023ac3c4
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-07 16:31:21 -0500

    Use the truncate function to shorten long movie overview descriptions in the banner to 150 characters

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -51,7 +51,9 @@ function Banner() {
           <button className="banner__button">My List</button>
         </div>
 
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
