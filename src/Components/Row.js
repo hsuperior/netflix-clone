1541d79c395e5cfee9dab9e0474e061834ae7277
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-12 17:34:25 -0500

    add movie trailers into the Row component

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "../Styles/Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const posterBaseUrl = "https://image.tmdb.org/t/p/w500/";
 
@@ -8,6 +10,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
   // STATE: This is basically how you write variables in React
   // Think of state as short-term memory
   const [movies, setMovies] = useState([]);
+
+  // Set trailerUrl Variable
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a specific condition
   useEffect(() => {
@@ -21,6 +26,30 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  // Function that runs when user clicks on picture
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log("Movie:", movie);
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=8Qn_spdM5Zg
+          const urlParams = new URLSearchParams(new URL(url).search);
+          // This gets the "v" value, which in this case would be 8Qn_spdM5Zg
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -29,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${posterBaseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -37,6 +67,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
